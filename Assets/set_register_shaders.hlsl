b41c16128585da258defae035702fa697f63bb3a
Powerpowername <1361581236@qq.com>
2025-07-27 15:40:55 +0800

    添加相关shader

---
+++ Assets/set_register_shaders.hlsl
@@ -0,0 +1,25 @@
+cbuffer SceneConstantBuffer : register(b0)
+{
+    float4x4 WorldViewProj;
+}
+
+struct PSInput
+{
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+};
+
+PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
+{
+    PSInput result;
+
+    result.position = mul(position, WorldViewProj);
+    result.color = color;
+
+    return result;
+}
+
+float4 PSMain(PSInput input) : SV_TARGET
+{
+    return input.color;
+}
