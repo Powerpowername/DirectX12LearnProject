30846e8b647f036829aa215c0209a50b7cdf1d3c
Powerpowername <1361581236@qq.com>
2025-08-31 21:39:42 +0800

    继续添加模板测试工厂代码

---
+++ StencilApp/GameTimer.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <Windows.h>
+
+class GameTimer
+{
+	GameTimer();
+
+	float TotalTime()const; // in seconds
+	float DeltaTime()const; // in seconds
+
+	void Reset(); // Call before message loop.
+	void Start(); // Call when unpaused.
+	void Stop();  // Call when paused.
+	void Tick();  // Call every frame.
+
+
+private:
+	double mSecondsPerCount;
+	double mDeltaTime;
+
+	__int64 mBaseTime;
+	__int64 mPausedTime;
+	__int64 mStopTime;
+	__int64 mPrevTime;
+	__int64 mCurrTime;
+
+	bool mStopped;
+};
+
+inline GameTimer::GameTimer() : mSecondsPerCount(0.0), mDeltaTime(-1.0), mBaseTime(0),
+mPausedTime(0), mPrevTime(0), mCurrTime(0), mStopped(false)
+{
+
+}
