8d5640cb7e989c67d0b777deae9a3ff1c43a29a0
Powerpowername <1361581236@qq.com>
2025-09-03 21:59:57 +0800

    解决方案的更新

---
+++ Packaging_DirectX12/packaging.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<Windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <wrl.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include<iostream>
+
+#include "directx\d3dx12.h"
+
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+
+using namespace Microsoft::WRL;
+using namespace DirectX;
+
+class D3DApp
+{
+
+};
